5a171f0af6cd36eef67c8b92acecf4dda9f00108
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-27 14:46:47 -0700

    modificacion de empleado dto

---
+++ dto_negocios/src/main/java/dto/EmpleadoDTO.java
@@ -0,0 +1,55 @@
+package dto;
+
+//@author SAUL ISAAC APODACA BALDENEGRO 00000252020
+
+public class EmpleadoDTO {
+
+    private String nombres;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String sucursal;
+    private String ocupacion;
+    private String urlFoto;
+
+    public EmpleadoDTO(String nombres, String apellidoPaterno, String apellidoMaterno, String sucursal, String ocupacion, String urlFoto) {
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.sucursal = sucursal;
+        this.ocupacion = ocupacion;
+        this.urlFoto = urlFoto;
+    }
+
+    public EmpleadoDTO(String nombres, String apellidoPaterno, String sucursal, String ocupacion, String urlFoto) {
+        this.nombres = nombres;
+        this.apellidoPaterno = apellidoPaterno;
+        this.sucursal = sucursal;
+        this.ocupacion = ocupacion;
+        this.urlFoto = urlFoto;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public String getSucursal() {
+        return sucursal;
+    }
+
+    public String getOcupacion() {
+        return ocupacion;
+    }
+
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+    
+}
