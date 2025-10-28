b0a5c709ec061a95e5e50598d0d6e23b7aebc7ba
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-27 17:56:49 -0700

    agrego proyectos subsistemas
    SOLO SON LOS PROYECTOS AGREGADOS

---
+++ negocios_VenderBoleto/src/main/java/itson/negocios_venderboleto/Negocios_VenderBoleto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package itson.negocios_venderboleto;
+
+/**
+ *
+ * @author saula
+ */
+public class Negocios_VenderBoleto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
