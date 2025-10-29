911be2269b0b6b8db500f34ca2ea9daf19562ba3
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-29 03:12:40 -0700

    subsistema caso principal mock, tiene fabrica y fachada

---
+++ negocios_VenderBoleto/src/main/java/itson/negocios_venderboleto/fabrica/GestorFactory.java
@@ -0,0 +1,71 @@
+package itson.negocios_venderboleto.fabrica;
+
+//@author SAUL ISAAC APODACA BALDENEGRO 00000252020
+
+import itson.negocios_correoelectronico.CorreoElectronico;
+import itson.negocios_correoelectronico.ICorreoElectronico;
+import itson.negocios_generadorqr.GeneradorQR;
+import itson.negocios_generadorqr.IGeneradorQR;
+import itson.negocios_gestorasientos.GestorAsientos;
+import itson.negocios_gestorasientos.IGestorAsientos;
+import itson.negocios_gestorboletos.GestorBoletos;
+import itson.negocios_gestorboletos.IGestorBoletos;
+import itson.negocios_gestorempleados.GestorEmpleados;
+import itson.negocios_gestorempleados.IGestorEmpleados;
+import itson.negocios_gestorfunciones.GestorFunciones;
+import itson.negocios_gestorfunciones.IGestorFunciones;
+import itson.negocios_gestormembresias.GestorMembresias;
+import itson.negocios_gestormembresias.IGestorMembresias;
+import itson.negocios_gestorpeliculas.GestorPeliculas;
+import itson.negocios_gestorpeliculas.IGestorPeliculas;
+import itson.negocios_gestorventas.GestorVentas;
+import itson.negocios_gestorventas.IGestorVentas;
+
+public class GestorFactory implements IGestorFactory {
+
+    @Override
+    public IGestorPeliculas crearGestorPeliculas() {
+        return new GestorPeliculas();
+    }
+
+    @Override
+    public IGestorFunciones crearGestorFunciones() {
+        return new GestorFunciones();
+    }
+
+    @Override
+    public IGestorAsientos crearGestorAsientos() {
+        return new GestorAsientos();
+    }
+
+    @Override
+    public IGestorEmpleados crearGestorEmpleados() {
+        return new GestorEmpleados();
+    }
+
+    @Override
+    public IGestorMembresias crearGestorMembresias() {
+        return new GestorMembresias();
+    }
+
+    @Override
+    public IGestorVentas crearGestorVentas() {
+        return new GestorVentas();
+    }
+
+    @Override
+    public IGestorBoletos crearGestorBoletos() {
+        return new GestorBoletos();
+    }
+
+    @Override
+    public IGeneradorQR crearGeneradorQR() {
+        return new GeneradorQR();
+    }
+
+    @Override
+    public ICorreoElectronico crearCorreoElectronico() {
+        return new CorreoElectronico();
+    }
+
+}
---
+++ negocios_VenderBoleto/src/main/java/itson/negocios_venderboleto/fachada/VentaBoletoFacade.java
@@ -0,0 +1,99 @@
+package itson.negocios_venderboleto.fachada;
+
+//@author SAUL ISAAC APODACA BALDENEGRO 00000252020
+
+import dto.AsientoDTO;
+import dto.BoletoDTO;
+import dto.EmpleadoDTO;
+import dto.FiltroDTO;
+import dto.FuncionDTO;
+import dto.MembresiaDTO;
+import dto.PeliculaDTO;
+import dto.PromocionDTO;
+import dto.VentaDTO;
+import itson.negocios_gestorasientos.IGestorAsientos;
+import itson.negocios_gestorempleados.IGestorEmpleados;
+import itson.negocios_gestorfunciones.IGestorFunciones;
+import itson.negocios_gestormembresias.IGestorMembresias;
+import itson.negocios_gestorpeliculas.IGestorPeliculas;
+import itson.negocios_gestorventas.IGestorVentas;
+import itson.negocios_venderboleto.IVentaBoleto;
+import itson.negocios_venderboleto.VentaBoleto;
+import itson.negocios_venderboleto.fabrica.GestorFactory;
+import java.util.Date;
+import java.util.List;
+
+public class VentaBoletoFacade implements IVentaBoletoFacade {
+
+    private final IVentaBoleto ventaBoleto;
+    private final IGestorPeliculas gestorPeliculas;
+    private final IGestorFunciones gestorFunciones;
+    private final IGestorAsientos gestorAsientos;
+    private final IGestorEmpleados gestorEmpleados;
+    private final IGestorMembresias gestorMembresias;
+    private final IGestorVentas gestorVentas;
+
+    public VentaBoletoFacade(GestorFactory factory) {
+        this.gestorPeliculas = factory.crearGestorPeliculas();
+        this.gestorFunciones = factory.crearGestorFunciones();
+        this.gestorAsientos = factory.crearGestorAsientos();
+        this.gestorEmpleados = factory.crearGestorEmpleados();
+        this.gestorMembresias = factory.crearGestorMembresias();
+        this.gestorVentas = factory.crearGestorVentas();
+
+        IVentaBoleto venta = new VentaBoleto(
+                gestorVentas,
+                factory.crearGestorBoletos(),
+                gestorAsientos,
+                factory.crearGeneradorQR(),
+                factory.crearCorreoElectronico()
+        );
+        this.ventaBoleto = venta;
+    }
+
+    @Override
+    public BoletoDTO venderBoleto(FuncionDTO funcion, List<AsientoDTO> asientosSeleccionados, EmpleadoDTO empleado, MembresiaDTO membresia, PromocionDTO promoSeleccionada) {
+        return ventaBoleto.venderBoleto(funcion, asientosSeleccionados, empleado, membresia, promoSeleccionada);
+    }
+
+    @Override
+    public void mandarBoletoCorreo(BoletoDTO boleto, String correo) {
+        ventaBoleto.mandarBoletoCorreo(boleto, correo);
+    }
+
+    @Override
+    public List<PeliculaDTO> obtenerPeliculas(FiltroDTO filtro) {
+        return gestorPeliculas.listarCartelera(filtro);
+    }
+
+    @Override
+    public List<FuncionDTO> listarFunciones(PeliculaDTO pelicula, Date fecha) {
+        return gestorFunciones.obtenerFuncionesPorPelícula(pelicula, fecha);
+    }
+
+    @Override
+    public List<AsientoDTO> obtenerAsientos(FuncionDTO funcion) {
+        return gestorAsientos.obtenerAsientos(funcion);
+    }
+
+    @Override
+    public EmpleadoDTO obtenerEmpleadoSesion(String idEmpleado) {
+        return gestorEmpleados.obtenerSesion();
+    }
+
+    @Override
+    public List<PromocionDTO> listarPromocionesMembresia(MembresiaDTO membresia) {
+        return gestorMembresias.obtenerPromociones(membresia.getCodigoMembresia());
+    }
+
+    @Override
+    public MembresiaDTO verificarMembresia(String codigo) {
+        return gestorMembresias.buscarMembresia(codigo);
+    }
+
+    @Override
+    public VentaDTO obtenerVentaPorId(String idVenta) {
+        return gestorVentas.obtenerVentaPorID(idVenta);
+    }
+
+}
