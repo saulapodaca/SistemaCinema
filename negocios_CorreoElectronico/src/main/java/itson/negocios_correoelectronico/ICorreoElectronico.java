c763590ee759b57b0b11eca40093bf9eef5806f7
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-29 03:12:04 -0700

    subsistema correo electronico mock

---
+++ negocios_CorreoElectronico/src/main/java/itson/negocios_correoelectronico/ICorreoElectronico.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package itson.negocios_correoelectronico;
+
+import dto.BoletoDTO;
+
+/**
+ *
+ * @author saula
+ */
+public interface ICorreoElectronico {
+    void enviarBoleto(String correoDestino, BoletoDTO boleto);
+}
