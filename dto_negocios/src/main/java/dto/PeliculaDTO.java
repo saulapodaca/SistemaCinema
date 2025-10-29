c39d6270f958c20bcbb5726b0948abdb821d4862
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-29 03:08:37 -0700

    modifico dto de pelicula para agreagr id

--- dto_negocios/src/main/java/dto/PeliculaDTO.java
+++ dto_negocios/src/main/java/dto/PeliculaDTO.java
@@ -3,18 +3,24 @@ package dto;
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 public class PeliculaDTO {
 
+    private String id;
     private String titulo;
     private String rutaImagen;
     private String duracion;
     private String idioma;
 
-    public PeliculaDTO(String titulo, String rutaImagen, String duracion, String idioma) {
+    public PeliculaDTO(String id, String titulo, String rutaImagen, String duracion, String idioma) {
+        this.id = id;
         this.titulo = titulo;
         this.rutaImagen = rutaImagen;
         this.duracion = duracion;
         this.idioma = idioma;
     }
 
+    public String getId(){
+        return id;
+    }
+    
     public String getTitulo() {
         return titulo;
     }
