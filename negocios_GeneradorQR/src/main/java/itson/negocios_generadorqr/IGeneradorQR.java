3c5eb9ded54a55499081cccde51192fbf207b274
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-29 03:11:47 -0700

    subsistema generador qr mock

---
+++ negocios_GeneradorQR/src/main/java/itson/negocios_generadorqr/IGeneradorQR.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package itson.negocios_generadorqr;
+
+/**
+ *
+ * @author saula
+ */
+public interface IGeneradorQR {
+    String generarQR(String contenido);
+}
