0613d3c2e8da9b0dbba556ebd4f774f5959c4214
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-10-30 18:46:25 -0700

    agrego dto del detalle del precio

---
+++ dto_negocios/src/main/java/dto/DetallePrecioDTO.java
@@ -0,0 +1,28 @@
+package dto;
+
+//@author SAUL ISAAC APODACA BALDENEGRO 00000252020
+
+public class DetallePrecioDTO {
+
+    private double subtotal;
+    private double descuento;
+    private double total;
+
+    public DetallePrecioDTO(double subtotal, double descuento, double total) {
+        this.subtotal = subtotal;
+        this.descuento = descuento;
+        this.total = total;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+}
