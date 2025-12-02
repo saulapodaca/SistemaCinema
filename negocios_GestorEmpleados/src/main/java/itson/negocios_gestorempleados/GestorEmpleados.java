729c5885be52d9216335d944d673714bee548574
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-12-01 19:40:28 -0700

    subsistema gestor empleados

--- negocios_GestorEmpleados/src/main/java/itson/negocios_gestorempleados/GestorEmpleados.java
+++ negocios_GestorEmpleados/src/main/java/itson/negocios_gestorempleados/GestorEmpleados.java
@@ -2,13 +2,26 @@ package itson.negocios_gestorempleados;
 
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
+import bos.EmpleadoBO;
 import dto.EmpleadoDTO;
+import dto.SesionDTO;
+import exceptions.EmpleadoNoEncontradoException;
+import exceptions.SesionEmpleadoNoExistenteException;
+import exceptions.SucursalNoExistenteException;
+import interfaces.IEmpleadoBO;
 
 public class GestorEmpleados implements IGestorEmpleados {
 
+    private final IEmpleadoBO empleadoBO = EmpleadoBO.getInstancia();
+    
     @Override
-    public EmpleadoDTO obtenerSesion(String idEmpleado) {
-        return new EmpleadoDTO(idEmpleado, "Saul", "Apodaca", "Nainari", "Gerente", "/fotoDefault.png");
+    public EmpleadoDTO obtenerPorId(String idEmpleado) throws EmpleadoNoEncontradoException, SucursalNoExistenteException {
+        return empleadoBO.obtenerPorId(idEmpleado);
+    }
+
+    @Override
+    public EmpleadoDTO obtenerSesionEmpleado(SesionDTO sesion) throws SesionEmpleadoNoExistenteException, SucursalNoExistenteException  {
+        return empleadoBO.obtenerSesionEmpleado(sesion);
     }
 
 }
