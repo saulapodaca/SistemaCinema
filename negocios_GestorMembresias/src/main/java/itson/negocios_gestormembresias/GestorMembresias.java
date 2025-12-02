53d6b9a332503ae579a6bc8f9134645c9f241920
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-12-01 19:40:51 -0700

    subsistema gestor membresias

--- negocios_GestorMembresias/src/main/java/itson/negocios_gestormembresias/GestorMembresias.java
+++ negocios_GestorMembresias/src/main/java/itson/negocios_gestormembresias/GestorMembresias.java
@@ -2,13 +2,16 @@ package itson.negocios_gestormembresias;
 
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
+import bos.MembresiaBO;
 import dto.MembresiaDTO;
-import java.util.Date;
+import interfaces.IMembresiaBO;
 
 public class GestorMembresias implements IGestorMembresias {
 
+    private final IMembresiaBO membresiaBO = MembresiaBO.getInstancia();
+    
     @Override
     public MembresiaDTO buscarMembresia(String codigoMembresia) {
-        return new MembresiaDTO(codigoMembresia, MembresiaDTO.TipoMembresia.ESTUDIANTE, "Saul Apodaca", new Date(), true);
+        return membresiaBO.buscarMembresia(codigoMembresia);
     }
 }
