44881f22e04993651545d3653f1b2a4fd2b454e1
egho22 <alejandro.rodriguez251622@potros.itson.edu.mx>
2025-12-04 16:24:13 -0700

    correcion bug funcion

--- presentacion_SistemaCinema/src/main/java/presentacion/PanelSeleccionFuncion.java
+++ presentacion_SistemaCinema/src/main/java/presentacion/PanelSeleccionFuncion.java
@@ -6,7 +6,9 @@ import dto.FuncionDTO;
 import dto.PeliculaDTO;
 import exceptions.FuncionNoEncontradaException;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Image;
+import java.awt.event.MouseListener;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -31,7 +33,7 @@ public class PanelSeleccionFuncion extends javax.swing.JPanel {
     private List<JLabel> etiquetasTipoSala;
     private List<JPanel> panelesFunciones;
     private PeliculaDTO pelicula;
-    private final int TAMANO_PAGINA = 4; 
+    private final int TAMANO_PAGINA = 4;
 
     /**
      * Creates new form PanelSeleccionFuncion
@@ -433,7 +435,7 @@ public class PanelSeleccionFuncion extends javax.swing.JPanel {
                 JLabel lblTipoSala = etiquetasTipoSala.get(i);
                 JPanel pnlFuncion = panelesFunciones.get(i);
 
-                limpiarEtiquetas(lblNombreSala, lblHoraFuncion, lblTipoSala);
+                limpiarEtiquetas(pnlFuncion, lblNombreSala, lblHoraFuncion, lblTipoSala);
 
                 if (i < funciones.size()) {
                     FuncionDTO funcion = funciones.get(i);
@@ -442,8 +444,8 @@ public class PanelSeleccionFuncion extends javax.swing.JPanel {
                 }
             }
 
-        }catch (FuncionNoEncontradaException e){
-            JOptionPane.showMessageDialog(this, e.getMessage(), "ERROR",JOptionPane.ERROR_MESSAGE);
+        } catch (FuncionNoEncontradaException e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -463,12 +465,12 @@ public class PanelSeleccionFuncion extends javax.swing.JPanel {
      */
     private void mostrarFuncionEtiqueta(FuncionDTO funcion, JLabel lblNombreSala, JLabel lblHoraFuncion, JLabel lblTipoSala, JPanel panelFuncion) {
         configurarNombreSala(lblNombreSala, funcion.getSala().getNombre());
-        
+
         LocalDateTime fechaHora = funcion.getFechaHora();
         String hora = fechaHora.toLocalTime()
-        .format(DateTimeFormatter.ofPattern("hh:mm a")); 
+                .format(DateTimeFormatter.ofPattern("hh:mm a"));
         configurarHoraFuncion(lblHoraFuncion, hora);
-        
+
         configurarTipoSala(lblTipoSala, funcion.getTipoSala());
         configurarEventoClick(panelFuncion, funcion);
     }
@@ -555,10 +557,14 @@ public class PanelSeleccionFuncion extends javax.swing.JPanel {
      * @param lblHoraFuncion etiqueta dónde va ala hora de la función
      * @param lblTipoSala etiqueta dónde va el tipo de sala de la función
      */
-    private void limpiarEtiquetas(JLabel lblNombreSala, JLabel lblHoraFuncion, JLabel lblTipoSala) {
+    private void limpiarEtiquetas(JPanel panelFuncion, JLabel lblNombreSala, JLabel lblHoraFuncion, JLabel lblTipoSala) {
         lblNombreSala.setText("");
         lblHoraFuncion.setText("");
         lblTipoSala.setText("");
+        for (MouseListener ml : panelFuncion.getMouseListeners()) {
+            panelFuncion.removeMouseListener(ml);
+        }
+        panelFuncion.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
