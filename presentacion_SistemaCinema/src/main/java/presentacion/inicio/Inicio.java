029fe5ef04a3d4b39983a037505623d3b1a11686
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-12-03 17:11:53 -0700

    presentacion lista

--- presentacion_SistemaCinema/src/main/java/presentacion/inicio/Inicio.java
+++ presentacion_SistemaCinema/src/main/java/presentacion/inicio/Inicio.java
@@ -2,13 +2,14 @@ package presentacion.inicio;
 
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
+import presentacion.controles.ControlInsertMasivo;
 import presentacion.controles.ControlPantallas;
 
 
 public class Inicio {
 
     public static void main(String[] args) {
-        
+        ControlInsertMasivo.getInstance().insertMasivo();
         ControlPantallas control = ControlPantallas.getInstance();
         control.abrirInicioSesion();
     }
