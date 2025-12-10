f44e63d4af650b727f15b698c11deca57395494c
kai-Akagi <hector.alonso252039@potros.itson.edu.mx>
2025-12-10 07:15:28 -0700

    a

--- negocios_GestorCanjearBeneficiosK/src/main/java/itson/negocios_gestorcanjearbeneficiosk/GestorCanjearBeneficios.java
+++ negocios_GestorCanjearBeneficiosK/src/main/java/itson/negocios_gestorcanjearbeneficiosk/GestorCanjearBeneficios.java
@@ -10,6 +10,10 @@ import dominio.Membresia;
 import dto.MembresiaDTO;
 import dto.ObjetoBeneficioDTO;
 import interfaces.IObjetoBeneficioBO;
+import itson.negocios_gestorObjetosBeneficio.GestorObjetosBeneficio;
+import itson.negocios_gestorObjetosBeneficio.IGestorObjetosBeneficio;
+import itson.negocios_gestormembresias.GestorMembresias;
+import itson.negocios_gestormembresias.IGestorMembresias;
 import java.util.List;
 
 /**
@@ -18,25 +22,27 @@ import java.util.List;
  */
 public class GestorCanjearBeneficios implements IGestorCanjearBeneficiosK {
     
+    private final IGestorMembresias fachadaMem = new GestorMembresias();
+    private final IGestorObjetosBeneficio fachadaObj = new GestorObjetosBeneficio();
     
     @Override
     public MembresiaDTO buscarMembresia (String codigoMembresia){
-       
+       return fachadaMem.buscarMembresia(codigoMembresia);
     }
     
     @Override
     public boolean actualizarPuntos(String codigomembresia, int saldonuevo){
-        
+        return fachadaMem.actualizarPuntos(codigomembresia, saldonuevo);
     }
     
     @Override
     public boolean actualizarStock(String idOB, int unidades_vendidas){
-         return fachada.actualizarStock(idOB, unidades_vendidas);
+         return fachadaObj.actualizarStock(idOB, unidades_vendidas);
      }
     
     @Override
     public List<ObjetoBeneficioDTO> obtenerObjetosDisponiblesPorMembresia(Membresia membresia){
-        return fachada.obtenerObjetosDisponiblesPorMembresia(membresia);
+        return fachadaObj.obtenerObjetosDisponiblesPorMembresia(membresia);
     }
     
     
