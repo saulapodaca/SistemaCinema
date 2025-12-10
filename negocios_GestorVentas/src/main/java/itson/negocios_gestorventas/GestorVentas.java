e9d4da0e040d7767dff644daafa58a09a4e33aaf
saulapodaca <saul.apodaca252020@potros.itson.edu.mx>
2025-12-10 16:02:25 -0700

    gestorVentas con cu venderalimento integrado

--- negocios_GestorVentas/src/main/java/itson/negocios_gestorventas/GestorVentas.java
+++ negocios_GestorVentas/src/main/java/itson/negocios_gestorventas/GestorVentas.java
@@ -2,21 +2,27 @@ package itson.negocios_gestorventas;
 
 //@author SAUL ISAAC APODACA BALDENEGRO 00000252020
 
+import bos.VentaAlimentoBO;
 import bos.VentaBO;
 import dto.AsientoDTO;
+import dto.ComboDTO;
 import dto.DetallePrecioDTO;
 import dto.PromocionDTO;
+import dto.VentaAlimentoDTO;
 import dto.VentaDTO;
+import exceptions.ComboException;
 import exceptions.EmpleadoNoEncontradoException;
 import exceptions.FuncionNoEncontradaException;
 import exceptions.SucursalNoExistenteException;
 import exceptions.VentaNoEncontradaException;
+import interfaces.IVentaAlimentoBO;
 import interfaces.IVentaBO;
 import java.util.List;
 
 public class GestorVentas implements IGestorVentas {
 
     private static IVentaBO ventaBO = VentaBO.getInstancia();
+    private static IVentaAlimentoBO ventaAlimentoBO = VentaAlimentoBO.getInstancia();
 
     @Override
     public VentaDTO registrarVenta(VentaDTO venta) throws EmpleadoNoEncontradoException, Exception{
@@ -31,5 +37,19 @@ public class GestorVentas implements IGestorVentas {
     @Override
     public DetallePrecioDTO calcularPrecios(List<AsientoDTO> asientos, PromocionDTO promo){
         return ventaBO.calcularPrecios(asientos, promo);
+    }
+
+    @Override
+    public VentaAlimentoDTO registrarVentaCombo(VentaAlimentoDTO venta) throws ComboException{
+        try{
+            return ventaAlimentoBO.registrarVentaCombo(venta);
+        } catch (ComboException | EmpleadoNoEncontradoException | SucursalNoExistenteException | VentaNoEncontradaException ex){
+            throw new ComboException(ex.getMessage());
+        }
+    }
+
+    @Override
+    public DetallePrecioDTO calcularPreciosCombo(ComboDTO combo) {
+        return ventaAlimentoBO.calcularPreciosCombo(combo);
     }
 }
